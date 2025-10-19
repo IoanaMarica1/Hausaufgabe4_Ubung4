2272391b3897c88be14ac2081f699171332f9350
user <ioanamarica66@gmail.com>
2025-10-19 19:57:11 +0300

    Final changes made for price checking.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int[] tastaturen = {40, 50, 60};
+        int[] tastaturen = {40, 50, 60, 30};
         int[] usb = {8, 12};
         int budget = 60;
+        for(int i = 0; i < tastaturen.length; i++){
+            if(tastaturen[i]<0){
+                System.out.println("Ungultiger Preis: "+tastaturen[i]);
+                return;
+            }
+        }
+        for(int i = 0; i < usb.length; i++){
+            if(usb[i]<0){
+                System.out.println("Ungultiger Preis: "+usb[i]);
+                return;
+            }
+        }
 
         ElektronikShop shop = new ElektronikShop(tastaturen, usb);
 
